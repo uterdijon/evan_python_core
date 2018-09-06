b6e980736ac19f16556822f5c844b4171d90f928
Evan <evan@MacBook.mshome.net>
2018-09-06 06:17:35 +0100

    Completed exercises

--- week_02/mini_projects/guess.py
+++ week_02/mini_projects/guess.py
@@ -9,3 +9,17 @@ pick the correct (randomly generated) number between 1 and 100.
 Tip: Use python's 'random' module.
 
 '''
+import random
+
+def guess_the_number():
+    guessed_correct = False
+    random_number = random.randint(1, 100)
+    while guessed_correct == False:
+        guessed_number = int(input("I am thinking of a number between 1 and 100. Guess the number: "))
+        if guessed_number == random_number:
+            print("Congratulations! You have guessed the correct number!")
+        else:
+            print("Sorry, that isn't the correct number.")
+            guessed_number = True
+
+guess_the_number()
