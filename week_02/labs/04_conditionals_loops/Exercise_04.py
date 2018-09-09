a788c8d656c5cdfa1a45edbd637eb88bcbcf47cb
Evan <evan@MacBook.home>
2018-09-09 20:48:33 +0100

    New solution for Exercise 4

--- week_02/labs/04_conditionals_loops/Exercise_04.py
+++ week_02/labs/04_conditionals_loops/Exercise_04.py
@@ -1,4 +1,22 @@
 '''
 Print out every prime number between 1 and 100.
 
-'''
+
+##############
+def is_prime(number):
+    prime = False
+    for i in range(2,number):
+        if number % i == 0:
+            print(number,"is a not a prime number.")
+            break
+        else:
+            prime = True
+    if prime == True:
+        print(number,"is a prime number.")
+
+def list_primes(upper):
+    for number in range(2,upper):
+        is_prime(number)
+
+list_primes(100)
+##############
