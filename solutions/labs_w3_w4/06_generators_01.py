b51c62f17dc5bc0f770733f203996bb0125a4630
martin-martin <breuss.martin@gmail.com>
2018-09-18 13:52:12 +0200

    improve wording

--- solutions/labs_w3_w4/06_generators_01.py
+++ solutions/labs_w3_w4/06_generators_01.py
@@ -4,7 +4,7 @@ the items that are divisible by 1111.
 
 '''
 
-# remember: range() also creates a generator object (try printing it!)
+# note: range() also works with a generator object internally
 nums = range(1, 1000000)
 
 # creating the generator expression
